53491293b1e2b1604658babb8ef4dd75db86b4fc
Expo <crdailey01@gmail.com>
2023-07-28 23:38:34 -0700

    come bcack to tomorrow

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    class Solution {
+        public int missingNumber(int[] nums) {
+
+        }
+    }
+}
